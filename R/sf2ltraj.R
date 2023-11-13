41f6b9f8960fc60c990ae43d569fa8d8c04a8c1b
Jed Long <jed.long@uwo.ca>
2023-11-13 21:10:02 +0000

    version 0.5.1

--- R/sf2ltraj.R
+++ R/sf2ltraj.R
@@ -11,7 +11,6 @@
 #' @param sfp an object of the class \code{sf} which contains the time-stamped movement fixes (as points) of the object(s).
 #' @param date the name of the column containing the date-time information of each fix. Must be converted to a POSIXct type.
 #' @param id  either a character string indicating the identity of the animal or the name of the column with the ID of the individuals.
-#' @param cols (optional) character vector specifiying the names of columns to keep as attributes (infolocs) in the ltraj object.
 #'
 #' @return
 #' A \code{ltraj} object. For more information on objects of this type see \code{help(ltraj)} and \code{?as.ltraj}.
@@ -27,7 +26,7 @@
 #' @export
 #
 
-sf2ltraj <- function(sfp, date, id, cols=NULL){
+sf2ltraj <- function(sfp, date, id){
   
   #get coords
   xy = data.frame(st_coordinates(sfp))
@@ -44,16 +43,14 @@ sf2ltraj <- function(sfp, date, id, cols=NULL){
     id = id
   }
   
-  #get additional columns to append
-  if (!is.null(cols)){df = df[,cols]} else {df = NULL}
   
-  #create ltraj object\
+  #create ltraj object
   crs <- st_crs(sfp)
   if (is.na(crs)){
-    trj = as.ltraj(xy,date=datetime,id=id,infolocs=df)
+    trj = as.ltraj(xy,date=datetime,id=id)
   } else {
     prj4string <- crs$proj4string
-    trj = as.ltraj(xy,date=datetime,id=id,infolocs=df,proj4string=CRS(prj4string))
+    trj = as.ltraj(xy,date=datetime,id=id,proj4string=CRS(prj4string))
   }
   
   
